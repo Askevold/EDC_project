3ae6d90612a44dd686ff3b43946e0be4cfd570a5
TorunnR <46747424+TorunnR@users.noreply.github.com>
2020-04-21 10:34:28 +0200

    Update iris.py

--- iris.py
+++ iris.py
@@ -8,16 +8,20 @@ klasser = {
 }
 
 #deler opp dataen fra filene i et læresett (30) L[] og et treningssett (20) T[]
-def hent_data():
-    split = 30
-
+def hent_data(first):
     class_1 =open("class_1").read().split('\n')
     class_2= open("class_2").read().split('\n')
     class_3 = open("class_3").read().split('\n')
 
+    if (first):
+        split = 30
+        L = class_1[0:split] + class_2[0:split] + class_3[0:split]
+        T = class_1[split:50] + class_2[split:50] + class_3[split:50]
+    else:
+        split = 20
+        L = class_1[split:50] + class_2[split:50] + class_3[split:50]
+        T = class_1[0:split] + class_2[0:split] + class_3[0:split]
 
-    L = class_1[0:split] + class_2[0:split] + class_3[0:split]
-    T = class_1[split:50] + class_2[split:50] + class_3[split:50]
 
     LK = [0]*90
     TK = [0]*60
@@ -122,20 +126,42 @@ def printing_conf(conf):
         for j in range(3):
             print(conf[i][j], " | ", end = '')
         print("\n")
-def main():
-    iterations = 800
-    alpha = 0.005
-    L, T, LK, TK = hent_data()
 
-    W, mse_verd = trening(alpha,iterations,L, LK)
-
-    pred = predict(W,T)
-    true = np.argmax(TK, axis=1)
-
+def printing_results(W,L,LK,T,TK):
+    print("Trainingset:")
+    pred = predict(W, L)
+    true = np.argmax(LK, axis=1)
     conf = confusion_mat(pred, true)
-    print(conf)
-    printing_conf(conf)
 
+    print("Confusion Matrix:")
+    printing_conf(conf)
+    print("Correct:", np.sum(pred == true) / len(pred), '\n')
+
+    print("Testset:")
+    pred = predict(W, T)
+    true = np.argmax(TK, axis=1)
+    conf = confusion_mat(pred, true)
+
+    print("Confusion Matrix:")
+    printing_conf(conf)
+    print("Correct:", np.sum(pred == true) / len(pred))
+
+def main():
+    iterations = 5000
+    alpha = 0.0005
+
+    print("------------- 30 first for training and 20 last for testing -------------")
+    L, T, LK, TK = hent_data(True)
+    W, mse_verd = trening(alpha,iterations,L, LK)
+    printing_results(W,L,LK,T,TK)
+
+
+    print("------------- 30 last for training and 20 first for testing -------------")
+    L, T, LK, TK = hent_data(False)
+    W, mse_verd = trening(alpha,iterations,L, LK)
+    printing_results(W, L, LK, T, TK)
+
+main()
 
 
 main()
